a42b1e80fc7c280cceee176c35a9e5f0f2c0d470
NetuxDevelopment <66493412+NetuxDevelopment@users.noreply.github.com>
2023-08-17 23:53:50 -0500

    Update

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-cors: {
-origin: [‘https://xxxxxx.github.io’, ‘http://localhost:5000’],
-methods: ‘GET,HEAD,PUT,PATCH,DELETE’
+{
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  maxAge: 86400,
+  credentials: true,
 }
