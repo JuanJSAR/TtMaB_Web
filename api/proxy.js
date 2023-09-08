d939f8e9dc4e7db1fe542827ecd18a6d926aa14b
NetuxDevelopment <66493412+NetuxDevelopment@users.noreply.github.com>
2023-09-08 02:16:46 -0500

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -26,7 +26,7 @@ module.exports = async (req, res) => {
 	
   } catch (error) {
     res.status(500).json({
-      error: error.message
+      error: error.message,
 	  reqbody: req.body
     });
   }
