493cd38d54f0313ee41c4574a98e8593b85aafb7
NetuxDevelopment <66493412+NetuxDevelopment@users.noreply.github.com>
2023-09-08 02:57:20 -0500

    up

--- api/proxy.js
+++ api/proxy.js
@@ -2,6 +2,9 @@ const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
   try {	
+	if (!req.body) {
+      throw new Error('El cuerpo de la solicitud está vacío o no es un objeto JSON válido');
+    }
     const { url } = req.body;
 
     if (!url || !url.startsWith('https://drive.google.com/')) {
@@ -18,10 +21,8 @@ module.exports = async (req, res) => {
 
 	 // Configura los encabezados de respuesta
     res.setHeader('Content-Type', response.headers.get('Content-Type')); // Utiliza el Content-Type original del archivo
-	// res.setHeader('Content-Type', 'application/octet-stream'); // Establece el tipo de contenido como application/octet-stream
     res.setHeader('Content-Disposition', 'attachment; filename="AssetBundle"');
 
-	console.log('pass');
     // Envía la respuesta al cliente
     res.status(200).send(data); // Aquí no es necesario JSON.stringify
   } catch (error) {
